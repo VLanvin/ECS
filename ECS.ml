135e0587022cd604a3f31e6980882c1a476e2189
VLanvin <victor.lanvin@ens-cachan.fr>
2014-09-26 20:38:14 +0200

    Added functor, parametrized ECS

--- ECS.ml
+++ ECS.ml
@@ -1,135 +1,152 @@
-(* Entities are just keys to access properties' hashtables 
- * Idea : create a module to allow for other types of entities ? *)
-type entity = int
-
-exception Property_Error of string 
-
-(* Non type-dependant signature for properties.
- * Allows for lists of properties *)
-module type BASEPROP = sig
-  val bind : entity -> unit
-  val rem  : entity -> unit
-end
-
-(* Signature for properties.
- * Just some basic accessors *)
-module type PROPERTY = sig
+(* Module representing keys aka entities
+ * Entities are just keys to access properties' hashtables *)
+module type KEY = sig
   type t
-  val bind : entity -> unit
-  val get  : entity -> t
-  val set  : entity -> t -> unit 
-  (* Like set, but returns the entity after modification.
-   * Allows for easy composition *)
-  val s    : t -> entity -> entity
-  val rem  : entity -> unit
+  val zero : t
+  val incr : t ref -> unit
 end
 
-(* Signature for components (groups of properties)
- * When you bind an entity to a component,
- * properties are added automatically.
- * Warning : values of properties must be initialized
- * before use, or a default value must be provided. *)
-module type COMPONENT = sig
-  val bind : entity -> unit
-  val b    : entity -> entity
-  val rem  : entity -> unit
-  val bound: entity -> bool
-  val iter : (entity -> unit) -> unit
+(* Implementation of a basic key type *)
+module IntKey : KEY = struct
+  type t = int
+  let zero = 0
+  let incr = incr
 end
 
-let last_id = ref 0
+(* Functor to create the ECS system *)
+module Make (Key : KEY) = struct
 
-let next_id () = incr last_id; !last_id
+  exception Property_Error of string 
 
-let null_id = 0
+  type entity = Key.t
 
-(* Store components for easy deletion of entities *)
-let components : (module COMPONENT) list ref = ref []
-
-(* Delete an entity *)
-let delete ent = 
-  List.iter (fun (module C : COMPONENT) -> C.rem ent) !components
-
-(* Register a component *)
-let register_component c = components := c :: !components
-
-(* Creates a first-order module representing a component *)
-let new_component props deps = 
-  let entities = ref [] in
-  let module T = struct
-    let bound elt =
-      let rec aux = function
-        |[] -> false
-        |t::q when t = elt -> true
-        |t::q -> aux q
-      in aux !entities
-    let bind elt = 
-      if bound elt then () else begin 
-        entities := elt :: !entities;
-        List.iter
-        (fun (module P : BASEPROP) -> P.bind elt)
-        props; 
-        List.iter
-        (fun (module C : COMPONENT) -> C.bind elt)
-        deps
-      end
-    let b elt = bind elt; elt
-    let rem elt = List.iter 
-      (fun (module P : BASEPROP) -> P.rem elt)
-      props;
-      let rec aux = function 
-        |[] -> [] 
-        |t::q when t = elt -> q
-        |t::q -> t :: (aux q)
-      in entities := aux !entities
-    let iter f = List.iter f !entities
-  end in
-  register_component (module T : COMPONENT);
-  (module T : COMPONENT)
-
-(* Creates a first-order module representing a property *)
-let new_property (type s) ?default () = 
-  let table = Hashtbl.create 10 in
-  let module T = struct
-    type t = s
-    let bind elt =
-      try ignore (Hashtbl.find table elt)
-      with |Not_found -> Hashtbl.add table elt default 
-    let get elt =
-      try 
-        match Hashtbl.find table elt with
-        |None    -> raise (Property_Error "Not set (no default value)")
-        |Some(v) -> v
-      with |Not_found -> raise (Property_Error "Property not bound")
-    let set e v = Hashtbl.replace table e (Some v)
-    let s v e = set e v; e
-    let rem = Hashtbl.remove table
-  end in
-  (module T : PROPERTY with type t = s)
-
-let iterate func entities comps = 
-  let rec cfilter e = 
-    if List.fold_left 
-      (fun b (module C : COMPONENT) -> 
-       b && (C.bound e)) true comps
-    then func e
-  in List.iter cfilter entities
-      
-let iterate_all func = function
-  |[] -> raise (Property_Error "Can't iterate on empty components list")
-  |(module T : COMPONENT)::q -> begin
-    let rec cfilter e =
-      if List.fold_left
-        (fun b (module C : COMPONENT) ->
-        b && (C.bound e)) true q
-      then func e
-    in T.iter cfilter
+  (* Non type-dependant signature for properties.
+   * Allows for lists of properties *)
+  module type BASEPROP = sig
+    val bind : entity -> unit
+    val rem  : entity -> unit
   end
 
-let filter entities comps =
-  let l = ref [] in
-  iterate (fun e -> l := e::!l) entities comps; !l
+  (* Signature for properties.
+   * Just some basic accessors *)
+  module type PROPERTY = sig
+    type t
+    val bind : entity -> unit
+    val get  : entity -> t
+    val set  : entity -> t -> unit 
+    (* Like set, but returns the entity after modification.
+     * Allows for easy composition *)
+    val s    : t -> entity -> entity
+    val rem  : entity -> unit
+  end
 
-let filter_all comps = 
-  let l = ref [] in 
-  iterate_all (fun e -> l := e::!l) comps; !l
+  (* Signature for components (groups of properties)
+   * When you bind an entity to a component,
+   * properties are added automatically.
+   * Warning : values of properties must be initialized
+   * before use, or a default value must be provided. *)
+  module type COMPONENT = sig
+    val bind : entity -> unit
+    val b    : entity -> entity
+    val rem  : entity -> unit
+    val bound: entity -> bool
+    val iter : (entity -> unit) -> unit
+  end
+
+  let last_id = ref Key.zero
+
+  let next_id () = Key.incr last_id; !last_id
+
+  let null_id = Key.zero
+
+  (* Store components for easy deletion of entities *)
+  let components : (module COMPONENT) list ref = ref []
+
+  (* Delete an entity *)
+  let delete ent = 
+    List.iter (fun (module C : COMPONENT) -> C.rem ent) !components
+
+  (* Register a component *)
+  let register_component c = components := c :: !components
+
+  (* Creates a first-order module representing a component *)
+  let new_component props deps = 
+    let entities = ref [] in
+    let module T = struct
+      let bound elt =
+        let rec aux = function
+          |[] -> false
+          |t::q when t = elt -> true
+          |t::q -> aux q
+        in aux !entities
+      let bind elt = 
+        if bound elt then () else begin 
+          entities := elt :: !entities;
+          List.iter
+          (fun (module P : BASEPROP) -> P.bind elt)
+          props; 
+          List.iter
+          (fun (module C : COMPONENT) -> C.bind elt)
+          deps
+        end
+      let b elt = bind elt; elt
+      let rem elt = List.iter 
+        (fun (module P : BASEPROP) -> P.rem elt)
+        props;
+        let rec aux = function 
+          |[] -> [] 
+          |t::q when t = elt -> q
+          |t::q -> t :: (aux q)
+        in entities := aux !entities
+      let iter f = List.iter f !entities
+    end in
+    register_component (module T : COMPONENT);
+    (module T : COMPONENT)
+
+  (* Creates a first-order module representing a property *)
+  let new_property (type s) ?default () = 
+    let table = Hashtbl.create 10 in
+    let module T = struct
+      type t = s
+      let bind elt =
+        try ignore (Hashtbl.find table elt)
+        with |Not_found -> Hashtbl.add table elt default 
+      let get elt =
+        try 
+          match Hashtbl.find table elt with
+          |None    -> raise (Property_Error "Not set (no default value)")
+          |Some(v) -> v
+        with |Not_found -> raise (Property_Error "Property not bound")
+      let set e v = Hashtbl.replace table e (Some v)
+      let s v e = set e v; e
+      let rem = Hashtbl.remove table
+    end in
+    (module T : PROPERTY with type t = s)
+
+  let iterate func entities comps = 
+    let rec cfilter e = 
+      if List.fold_left 
+        (fun b (module C : COMPONENT) -> 
+        b && (C.bound e)) true comps
+      then func e
+    in List.iter cfilter entities
+        
+  let iterate_all func = function
+    |[] -> raise (Property_Error "Can't iterate on empty components list")
+    |(module T : COMPONENT)::q -> begin
+      let rec cfilter e =
+        if List.fold_left
+          (fun b (module C : COMPONENT) ->
+          b && (C.bound e)) true q
+        then func e
+      in T.iter cfilter
+    end
+
+  let filter entities comps =
+    let l = ref [] in
+    iterate (fun e -> l := e::!l) entities comps; !l
+
+  let filter_all comps = 
+    let l = ref [] in 
+    iterate_all (fun e -> l := e::!l) comps; !l
+end
--- example.ml
+++ example.ml
@@ -1,56 +1,58 @@
+module MyECS = ECS.Make(ECS.IntKey)
+
 (* Definition of 5 properties *)
 module Name = struct
   type s = string
-  include (val (ECS.new_property ~default:"toto" ())
-    : ECS.PROPERTY with type t = s)
+  include (val (MyECS.new_property ~default:"toto" ())
+    : MyECS.PROPERTY with type t = s)
 end
 
 module Age = struct
   type s = int
-  include (val (ECS.new_property ~default:18 ())
-    : ECS.PROPERTY with type t = s)
+  include (val (MyECS.new_property ~default:18 ())
+    : MyECS.PROPERTY with type t = s)
 end
 
 module Genre = struct
   type s = Male | Female
-  include (val (ECS.new_property ())
-    : ECS.PROPERTY with type t = s)
+  include (val (MyECS.new_property ())
+    : MyECS.PROPERTY with type t = s)
 end
 
 module Position = struct
   type s = (float * float)
-  include (val (ECS.new_property ~default:(0.,0.) ())
-    : ECS.PROPERTY with type t = s)
+  include (val (MyECS.new_property ~default:(0.,0.) ())
+    : MyECS.PROPERTY with type t = s)
 end
 
 module Speed = struct
   type s = (float * float)
-  include (val (ECS.new_property ~default:(0.,0.) ())
-    : ECS.PROPERTY with type t = s)
+  include (val (MyECS.new_property ~default:(0.,0.) ())
+    : MyECS.PROPERTY with type t = s)
 end
 
 (* Definition of 3 components *)
 module Living = struct
-  include (val (ECS.new_component 
+  include (val (MyECS.new_component 
     [(module Name );
      (module Age  );
      (module Genre)]
-    []) : ECS.COMPONENT)
+    []) : MyECS.COMPONENT)
 end
 
 module Movable = struct
-  include (val (ECS.new_component
+  include (val (MyECS.new_component
     [(module Position);
      (module Speed   )]
     [(module Living  )]
-  ) : ECS.COMPONENT)
+  ) : MyECS.COMPONENT)
 end
 
 module Thing = struct
-  include (val (ECS.new_component
+  include (val (MyECS.new_component
     [(module Name    );
      (module Position)]
-    []) : ECS.COMPONENT)
+    []) : MyECS.COMPONENT)
 end
 
 (* Definition of a system *)
@@ -69,7 +71,7 @@ end
 
 (* Test functions *)
 let create_sbdy genre name age pos spe = 
-  ECS.next_id ()
+  MyECS.next_id ()
   |> Living.b 
   |> Movable.b
   |> Genre.s genre
@@ -79,23 +81,23 @@ let create_sbdy genre name age pos spe =
   |> Speed.s spe
 
 let create_sbdy2 genre =
-  ECS.next_id ()
+  MyECS.next_id ()
   |> Movable.b
   |> Genre.s genre
 
 let create_sbdyfail () =
-  ECS.next_id ()
+  MyECS.next_id ()
   |> Movable.b
 
 let create_sbdy3 genre name age =
-  ECS.next_id ()
+  MyECS.next_id ()
   |> Living.b
   |> Genre.s genre
   |> Name.s name
   |> Age.s age
 
 let create_sbdy4 genre name age pos = 
-  ECS.next_id ()
+  MyECS.next_id ()
   |> Living.b 
   |> Movable.b
   |> Genre.s genre
@@ -109,32 +111,32 @@ let test_system () =
     create_sbdy2 Genre.Female;
     create_sbdy3 Genre.Male "titi" 22;
     create_sbdy4 Genre.Female "tata" 25 (0.,1.)]
-  in TestSystem.run (ECS.filter_all [(module Living); (module Movable)]);
-  List.iter ECS.delete l
+  in TestSystem.run (MyECS.filter_all [(module Living); (module Movable)]);
+  List.iter MyECS.delete l
 
 let test_exception () =
   let l = [create_sbdyfail ();
            create_sbdy2 Genre.Female]
   in try 
-    TestSystem.run (ECS.filter_all [(module Living); (module Movable)]);
-  with |ECS.Property_Error(_) -> print_endline "exception catched !";
-  List.iter ECS.delete l
+    TestSystem.run (MyECS.filter_all [(module Living); (module Movable)]);
+  with |MyECS.Property_Error(_) -> print_endline "exception catched !";
+  List.iter MyECS.delete l
 
 let test_remove () =
   let l = [
     create_sbdy2 Genre.Female;
     create_sbdy Genre.Male "tutu" 21 (3.,4.) (0.,0.)]
   in
-  TestSystem.run (ECS.filter_all [(module Living); (module Movable)]);
+  TestSystem.run (MyECS.filter_all [(module Living); (module Movable)]);
   Printf.printf "Removing entity...\n";
-  ECS.delete (List.hd l);
-  TestSystem.run (ECS.filter_all [(module Living); (module Movable)]);
-  ECS.delete (List.hd (List.tl l))
+  MyECS.delete (List.hd l);
+  TestSystem.run (MyECS.filter_all [(module Living); (module Movable)]);
+  MyECS.delete (List.hd (List.tl l))
 
 let test_persistance () =
   let (e1,e2) = 
     create_sbdy Genre.Male "titi" 22 (0.,0.) (2.,3.),
-    ECS.next_id () |> Thing.b
+    MyECS.next_id () |> Thing.b
   in 
   ignore (Thing.b e1
   |> Name.s "tititi");
@@ -142,8 +144,8 @@ let test_persistance () =
   |> Living.b
   |> Movable.b
   |> Genre.s Genre.Female);
-  TestSystem.run (ECS.filter_all [(module Living); (module Movable)]);
-  ECS.delete e1; ECS.delete e2
+  TestSystem.run (MyECS.filter_all [(module Living); (module Movable)]);
+  MyECS.delete e1; MyECS.delete e2
 
 let _ = 
   print_endline "Test 1 : System";
